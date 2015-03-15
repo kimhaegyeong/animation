16ee316e9b7942b225b8568668a941dc3441adcf
kimhaegyeong <schema09@gmail.com>
2015-03-15 09:32:29 +0900

    .

---
+++ parkinglot01/src/windowBuilder03/ParkingLot.java
@@ -0,0 +1,28 @@
+package windowBuilder03;
+
+public class ParkingLot {	
+	private boolean[][] plateLoation;
+	
+	ParkingLot() {
+		//plateLoation = new boolean[5][11];
+		plateLoation = new boolean[][] 	{
+					{false, true, true, true, true, false, true, true, true, true, false},
+					{false, true, true, true, true, false, true, true, true, true, false},
+					{false, true, true, true, true, false, true, true, true, true, false},
+					{false, true, true, true, true, false, true, true, true, true, false},
+					{false, true, true, true, true, false, true, true, true, true, false}
+				};		
+	}
+
+	public boolean[][] getPlateLoation() {
+		return plateLoation;
+	}
+
+	public void setPlateLoation(boolean[][] plateLoation) {
+		this.plateLoation = plateLoation;
+	}
+	
+	
+	
+	
+}
